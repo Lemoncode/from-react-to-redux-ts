bed0a016f6e8bd71941ba80c3e745173fe69bb5d
ebarriosara <estefania.1991@gmail.com>
2017-11-12 19:32:09 +0100

    01 Hardcoded list component
    - Add component to show a member in a row.
    - Add component to show a table with a row per member.
    - Change the page component to show a hardcoded list of members.
    - Define the view model.

--- 01 Hardcoded list component/src/pages/members/page.tsx
+++ 01 Hardcoded list component/src/pages/members/page.tsx
@@ -1,5 +1,23 @@
 import * as React from 'react';
+import { MemberEntity } from './viewModel';
+import { MemberTable } from './components';
 
-export const MemberListPage = () => (
-  <h1>Hello from member list page</h1>
-);
+interface Props {
+  memberList: MemberEntity[];
+  fetchMemberList: () => void;
+}
+
+export class MemberListPage extends React.Component<Props, {}> {
+
+  componentDidMount() {
+    this.props.fetchMemberList();
+  }
+
+  render() {
+    return (
+      <MemberTable
+        memberList={this.props.memberList}
+      />
+    );
+  }
+}
