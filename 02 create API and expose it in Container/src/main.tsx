13d0c6490cb39c2d8c185534b2740bd7198ae395
arp82 <alejandro.ro.pu@gmail.com>
2017-11-13 22:56:39 +0100

    Create folder structure based on 01 case

---
+++ 02 create API and expose it in Container/src/main.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { MemberListContainer } from './pages/members'; 
+
+ReactDOM.render(
+  <MemberListContainer />,
+  document.getElementById('root')
+);
